5d30a6414100124ae75f9c2cbaeb8d1d183b1dfc
nerutatarin <zarip-off@yandex.ru>
2022-05-01 09:51:24 +0500

    release-1.0.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,12 +12,14 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         for (int i = 0; i < 1; i++) {
-            List<Browsers> browsers = new ArrayList<>();
+            /*List<Browsers> browsers = new ArrayList<>();
             browsers.add(new Firefox());
             browsers.add(new Edge());
             browsers.add(new Chrome());
+            browsers.forEach(browser -> new VoteKP(browser).start());*/
 
-            browsers.forEach(browser -> new VoteKP(browser).start());
+            VoteImpl kp = new VoteKP(new Firefox());
+            kp.start();
 
             VoteImpl orgZdrav = new VoteOrgZdrav(new Firefox());
             orgZdrav.start();
