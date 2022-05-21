1c5956ea66ecb57dc0832c0280245a64ad3ed618
nerutatarin <zarip-off@yandex.ru>
2022-05-21 14:09:20 +0500

    release-1.0

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,7 @@ import vote.VoteImpl;
 import vote.browsers.Browsers;
 import vote.browsers.Firefox;
 import vote.browsers.MsEdge;
+import vote.browsers.Opera;
 import vote.vote2022.kp.VoteKP;
 
 import java.util.List;
@@ -11,7 +12,7 @@ import static java.util.Arrays.asList;
 public class Main {
     public static void main(String[] args) {
         for (int i = 0; i < 1; i++) {
-            List<Browsers> browsers = asList(new Firefox(), new MsEdge());
+            List<Browsers> browsers = asList(new Firefox(), new MsEdge(), new Opera());
             browsers.forEach(browser -> new VoteKP(browser).start());
 
             /*VoteImpl kp = new VoteKP(new MsEdge());
