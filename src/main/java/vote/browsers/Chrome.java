6aa1c495db979b46c66e57f70539e070bc945fc5
nerutatarin <zarip-off@yandex.ru>
2022-05-21 14:26:44 +0500

    release-1.0

--- src/main/java/vote/browsers/Chrome.java
+++ src/main/java/vote/browsers/Chrome.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+import static org.openqa.selenium.PageLoadStrategy.EAGER;
 import static org.openqa.selenium.PageLoadStrategy.NORMAL;
 import static org.openqa.selenium.chrome.ChromeDriverLogLevel.ALL;
 import static org.openqa.selenium.remote.CapabilityType.PAGE_LOAD_STRATEGY;
@@ -40,22 +41,20 @@ public class Chrome extends BrowsersImpl {
     private ChromeOptions getOptions() {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--enable-automation");
-        options.addArguments("--incognito");
+        options.addArguments("--headless");
         options.addArguments("--no-sandbox");
+        options.addArguments("--disable-update");
+        options.addArguments("--incognito");
         options.addArguments("--disable-gpu");
         options.addArguments("--ignore-ssl-errors");
         options.addArguments("--disable-extensions");
         options.addArguments("--dns-prefetch-disable");
         options.addArguments("--disable-popup-blocking");
-        options.addArguments("--ignore-certificate-errors");
         options.addArguments("--enable-precise-memory-info");
-        options.setCapability("--host-resolver-rules", "MAP * ~NOTFOUND, EXCLUDE " + PROXY_IP_ADDRESS + ":" + PROXY_PORT);
 
-        options.setLogLevel(ALL);
-        options.setHeadless(true);
         options.setAcceptInsecureCerts(true);
-        options.setCapability(PAGE_LOAD_STRATEGY, NORMAL);
-        options.setCapability(PROXY, getProxy());
+        options.setPageLoadStrategy(EAGER);
+        options.setProxy(getProxy());
         return options;
     }
 }
