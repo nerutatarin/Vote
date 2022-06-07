64a1866d15cb7470eef84f6168af5ed5a93a23e6
nerutatarin <zarip-off@yandex.ru>
2022-06-07 18:32:41 +0500

    release-1.0

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,7 +10,7 @@ public class Main {
     public static void main(String[] args) {
         for (int i = 0; i < 1; i++) {
 
-            int count = 1;
+            int count = 1000;
             boolean headless = true;
             boolean proxy = true;
 
